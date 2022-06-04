19338af35da1dca49ee91d0398a978b06264928d
Charlie Denton <charlie@meshy.co.uk>
2022-06-04 13:10:14 +0100

    Block all database connections, not just default

--- sans_db/context_managers.py
+++ sans_db/context_managers.py
@@ -1,7 +1,7 @@
-from contextlib import contextmanager
+from contextlib import ExitStack, contextmanager
 from typing import Iterator
 
-from django.db import connection
+from django.db import connections
 
 from .exceptions import DatabaseAccessBlocked
 
@@ -11,5 +11,11 @@ def block_db() -> Iterator[None]:
     def blocker(*args: object) -> None:
         raise DatabaseAccessBlocked
 
-    with connection.execute_wrapper(blocker):
+    databases = list(connections)
+    to_block = [connections[db_alias] for db_alias in databases]
+    managers = [connection.execute_wrapper(blocker) for connection in to_block]
+
+    with ExitStack() as stack:
+        for manager in managers:
+            stack.enter_context(manager)
         yield
