7074a12751a37629715cc72d0e8d915efd4dcbac
Charlie Denton <charlie@meshy.co.uk>
2022-06-06 22:46:36 +0100

    Define list of databases once based on settings

--- tests/template_backends/test_django_sans_db.py
+++ tests/template_backends/test_django_sans_db.py
@@ -5,6 +5,7 @@ from django.template.backends.django import Template
 from sans_db.exceptions import DatabaseAccessBlocked
 from sans_db.template_backends.django_sans_db import TemplateSansDB
 
+from ..definitions import DATABASE_ALIASES
 from ..models import ExampleModel
 
 
@@ -12,14 +13,14 @@ DJANGO_ENGINE = engines["django_sans_db"]
 TEMPLATE_STRING = "{% for item in items %}{{ item.pk }}{% endfor %}"
 
 
-@pytest.mark.django_db(databases=["mysql", "postgres", "sqlite"])
+@pytest.mark.django_db(databases=DATABASE_ALIASES)
 class TestDjangoTemplatesSansDB:
     def test_from_string_type(self) -> None:
         template = DJANGO_ENGINE.from_string(TEMPLATE_STRING)
         assert isinstance(template, Template)
         assert isinstance(template, TemplateSansDB)
 
-    @pytest.mark.parametrize("database", ["mysql", "postgres", "sqlite"])
+    @pytest.mark.parametrize("database", DATABASE_ALIASES)
     def test_query_in_template_string(self, database: str) -> None:
         context = {"items": ExampleModel.objects.using(database).all()}
         template = DJANGO_ENGINE.from_string(TEMPLATE_STRING)
@@ -27,7 +28,7 @@ class TestDjangoTemplatesSansDB:
         with pytest.raises(DatabaseAccessBlocked):
             template.render(context)
 
-    @pytest.mark.parametrize("database", ["mysql", "postgres", "sqlite"])
+    @pytest.mark.parametrize("database", DATABASE_ALIASES)
     def test_no_query_in_template_string(self, database: str) -> None:
         obj = ExampleModel.objects.using(database).create()
         context = {"items": list(ExampleModel.objects.using(database).all())}
@@ -42,7 +43,7 @@ class TestDjangoTemplatesSansDB:
         assert isinstance(template, Template)
         assert isinstance(template, TemplateSansDB)
 
-    @pytest.mark.parametrize("database", ["mysql", "postgres", "sqlite"])
+    @pytest.mark.parametrize("database", DATABASE_ALIASES)
     def test_query_in_template_file(self, database: str) -> None:
         obj = ExampleModel.objects.using(database).create()
         context = {"items": ExampleModel.objects.using(database).all()}
@@ -51,7 +52,7 @@ class TestDjangoTemplatesSansDB:
         with pytest.raises(DatabaseAccessBlocked):
             template.render(context)
 
-    @pytest.mark.parametrize("database", ["mysql", "postgres", "sqlite"])
+    @pytest.mark.parametrize("database", DATABASE_ALIASES)
     def test_no_query_in_template_file(self, database: str) -> None:
         obj = ExampleModel.objects.using(database).create()
         items = list(ExampleModel.objects.using(database).all())
--- tests/test_context_managers.py
+++ tests/test_context_managers.py
@@ -1,46 +1,37 @@
-from typing import Tuple
-
 import pytest
 
 from sans_db.context_managers import block_db
 from sans_db.exceptions import DatabaseAccessBlocked
 
+from .definitions import DATABASE_ALIASES
 from .models import ExampleModel
 
 
-@pytest.mark.django_db(databases=["mysql", "postgres", "sqlite"])
+@pytest.mark.django_db(databases=DATABASE_ALIASES)
 class TestBlockDB:
-    @pytest.mark.parametrize("database", ["mysql", "postgres", "sqlite"])
+    @pytest.mark.parametrize("database", DATABASE_ALIASES)
     def test_queryset_evaluation_blocked(self, database: str) -> None:
         queryset = ExampleModel.objects.using(database).all()
         with pytest.raises(DatabaseAccessBlocked):
             with block_db():
                 list(queryset)
 
-    @pytest.mark.parametrize("database", ["mysql", "postgres", "sqlite"])
+    @pytest.mark.parametrize("database", DATABASE_ALIASES)
     def test_evaluated_queryset_allowed(self, database: str) -> None:
         queryset = list(ExampleModel.objects.using(database).all())
         with block_db():
             for model in queryset:
                 pass
 
-    @pytest.mark.parametrize("blocked", ["mysql", "postgres", "sqlite"])
+    @pytest.mark.parametrize("blocked", DATABASE_ALIASES)
     def test_selective_blocking_blocked(self, blocked: str) -> None:
         with pytest.raises(DatabaseAccessBlocked):
             with block_db(databases=[blocked]):
                 ExampleModel.objects.using(blocked).create()
 
-    @pytest.mark.parametrize(
-        "blocked, not_blocked",
-        [
-            ["mysql", ("postgres", "sqlite")],
-            ["postgres", ("mysql", "sqlite")],
-            ["sqlite", ("mysql", "postgres")],
-        ],
-    )
-    def test_selective_blocking_allowed(
-        self, blocked: str, not_blocked: Tuple[str, str]
-    ) -> None:
+    @pytest.mark.parametrize("blocked", DATABASE_ALIASES)
+    def test_selective_blocking_allowed(self, blocked: str) -> None:
+        not_blocked = DATABASE_ALIASES - {blocked}
         # No error raised, because queries aren't on blocked DB.
         with block_db(databases=[blocked]):
             for db in not_blocked:
